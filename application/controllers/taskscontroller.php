cdc6af32b2ec9996b84fa955b553c1ea143d60d2
guruahn <guruahn@gmail.com>
2015-02-11 14:44:08 +0900

    이메일 발송 함수 추가(테스트)

--- application/controllers/taskscontroller.php
+++ application/controllers/taskscontroller.php
@@ -154,5 +154,94 @@ class TasksController extends Controller {
         }
     }
 
+    function sendMail(){
+        //Get your email address
+        // $contact_email = get_option('pp_contact_email');
+
+        //Enter your email address, email from contact form will send to this addresss. Please enter inside quotes ('myemail@email.com')
+        //define('DEST_EMAIL', $contact_email);
+
+        //Change email subject to something more meaningful
+        define('SUBJECT_EMAIL', __( '상담문의', THEMEDOMAIN ));
+
+        //Thankyou message when message sent
+        define('THANKYOU_MESSAGE', __( '감사합니다. 가능한 빠른 시일내에 답변드리겠습니다.', THEMEDOMAIN ));
+
+        //Error message when message can't send
+        define('ERROR_MESSAGE', __( 'Oops! something went wrong, please try to submit later.', THEMEDOMAIN ));
+
+
+        /*
+        |
+        | Begin sending mail
+        |
+        */
+
+        $from_name = $_POST['your_name'];
+        $from_email = $_POST['email'];
+
+        $mime_boundary_1 = md5(time());
+        $mime_boundary_2 = "1_".$mime_boundary_1;
+        $mail_sent = false;
+
+        # Common Headers
+        $headers = "";
+        $headers .= 'From: '.$from_name.'<webmaster@conkrit.com>'.PHP_EOL;
+        $headers .= 'Reply-To: '.$from_name.'<webmaster@conkrit.com>'.PHP_EOL;
+        $headers .= 'Return-Path: '.$from_name.'<webmaster@conkrit.com>'.PHP_EOL;        // these two to set reply address
+        $headers .= "Message-ID: <".$now."webmaster@".$_SERVER['SERVER_NAME'].">";
+        $headers .= "X-Mailer: PHP v".phpversion().PHP_EOL;                  // These two to help avoid spam-filters
+
+        $message = 'Name: '.$from_name.PHP_EOL;
+        $message.= 'Email: '.$from_email.PHP_EOL.PHP_EOL;
+        $message.= 'Message: '.PHP_EOL.$_POST['message'].PHP_EOL.PHP_EOL;
+
+        if(isset($_POST['address']))
+        {
+            $message.= 'Address: '.$_POST['address'].PHP_EOL;
+        }
+
+        if(isset($_POST['phone']))
+        {
+            $message.= 'Phone: '.$_POST['phone'].PHP_EOL;
+        }
+
+        if(isset($_POST['mobile']))
+        {
+            $message.= 'Mobile: '.$_POST['mobile'].PHP_EOL;
+        }
+
+        if(isset($_POST['company']))
+        {
+            $message.= 'Company: '.$_POST['company'].PHP_EOL;
+        }
+
+        if(isset($_POST['country']))
+        {
+            $message.= 'Country: '.$_POST['country'].PHP_EOL;
+        }
+
+
+        if(!empty($from_name) && !empty($from_email) && !empty($message))
+        {
+            mail(DEST_EMAIL, SUBJECT_EMAIL." : ".$from_name, $message, $headers);
+
+            echo THANKYOU_MESSAGE;
+
+            exit;
+        }
+        else
+        {
+            echo ERROR_MESSAGE;
+
+            exit;
+        }
+
+        /*
+        |
+        | End sending mail
+        |
+        */
+    }
 
 }
